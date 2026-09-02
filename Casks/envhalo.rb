73feeb60b21afacfa80310e3098246d573a5cec5
Dorjan95 <d.rudaj@codelab.email>
2026-09-03 00:21:54 +0200

    Release EnvHalo 0.2.0 with Sparkle updates

--- Casks/envhalo.rb
+++ Casks/envhalo.rb
@@ -1,11 +1,13 @@
 cask "envhalo" do
-  version "0.1.3"
-  sha256 "16042538754c1e659822ffd0142b228b0088d4a1cacdafde8517e1f1c8e3595e"
+  version "0.2.0"
+  sha256 "d73af4aed57703a38687c620fe941e014faf997c029e139e96892172cd18669f"
 
   url "https://github.com/Dorjan95/homebrew-envhalo/releases/download/v#{version}/EnvHalo-#{version}.zip"
   name "EnvHalo"
   desc "Ambient safety indicator for development environments"
   homepage "https://github.com/Dorjan95/homebrew-envhalo"
+
+  auto_updates true
 
   depends_on macos: :sonoma
 
