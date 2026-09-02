ae1c2ba39db4beb4b0a688278635088457962cb3
Dorjan95 <d.rudaj@codelab.email>
2026-09-02 21:45:44 +0200

    Update EnvHalo to 0.1.2

--- Casks/envhalo.rb
+++ Casks/envhalo.rb
@@ -1,6 +1,6 @@
 cask "envhalo" do
-  version "0.1.1"
-  sha256 "3f3c75efb36f09c9f7d760948ca4ed8c57aba0893d9042206938f8d711ae1b55"
+  version "0.1.2"
+  sha256 "238166fbec7853de0a6e6449ab085a0c8d7cfb511967b1761a3cc0adcd040be4"
 
   url "https://github.com/Dorjan95/homebrew-envhalo/releases/download/v#{version}/EnvHalo-#{version}.zip"
   name "EnvHalo"
